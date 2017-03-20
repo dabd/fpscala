7ff1bc316428423d50eda7899ad683d4c15e2061
Dario Abdulrehman <dario.rehman@gmail.com>
2017-03-20 14:17:00 +0000

    Implement 7.4

--- src/main/scala/fpinscala/parallelism/Par.scala
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -1,5 +1,41 @@
 package fpinscala.parallelism
 
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
 object Par {
 
+  // ex 7.1
+  //  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es =>
+      es.submit(new Callable[A] {
+        def call = a(es).get
+      })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  // ex 7.4
+  def asyncF[A,B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
 }
--- src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -1,5 +1,41 @@
 package fpinscala.parallelism
 
-class ParSpec {
+import java.util.concurrent._
 
+import fpinscala.CommonSpec
+import fpinscala.parallelism.Par.Par
+import org.scalacheck.{Arbitrary, Cogen, Gen}
+import org.scalatest.prop.Checkers
+
+class ParSpec extends CommonSpec with Checkers {
+
+  implicit val genExecutorService: Gen[ExecutorService] =
+    Gen.const(Executors.newFixedThreadPool(4))
+
+  implicit val arbExecutorService: Arbitrary[ExecutorService] =
+    Arbitrary(genExecutorService)
+
+  // The toString method of ThreadPoolExecutor reflects the internal state of the ExecutorService
+  // so we try to use it for the Cogen
+  implicit val cogenExecutorService: Cogen[ExecutorService] =
+    Cogen[String].contramap(_.toString)
+
+  implicit def genFuture[A]: Gen[Future[A]] =
+    Gen.const(new CompletableFuture[A]())
+
+  implicit def arbFuture[A](implicit a: Arbitrary[A]): Arbitrary[Future[A]] =
+    Arbitrary(genFuture)
+
+  implicit def cogenFuture[A](implicit a: Cogen[A]): Cogen[Future[A]] =
+    Cogen[String].contramap(_.toString)
+
+  implicit def arbitraryPar[A: Arbitrary]: Arbitrary[Par[A]] =
+    Arbitrary.arbFunction1[ExecutorService, Future[A]]
+
+  // ex 7.4
+  "asyncF" should "be" in {
+    forAll { (a: Int, f: Int => Int, es: ExecutorService) =>
+      Par.asyncF(f)(a)(es).get() mustBe f(a)
+    }
+  }
 }
